cd0ef4265406b3534e5770aedf95f2583ae0c5f5
Abhinav Goyal <agabohar@gmail.com>
2019-04-29 04:55:28 +0530

    scripts to run experiments

---
+++ test_i.sh
@@ -0,0 +1,9 @@
+cuda=$1
+
+for seed in 0 1 2 3 4; do
+  for ms in 50 100 150 200 250 300 350 400 450 500; do
+    python main.py -p $cuda -r $seed test\
+     -d data/mnist/test.csv\
+     models/gc$seed-500-$ms -i
+  done
+done
---
+++ train_all.sh
@@ -0,0 +1,35 @@
+cuda=$1
+seed=$2
+
+ds=925
+for ms in 278 370 463 555 648 740 833 925; do
+  for model in g c; do
+    python main.py -p $cuda -r $seed train -mc $model'.config'\
+    -d data/ms-celebs/data2/train$seed.csv -ds $ms\
+    -s train_specs.txt models/$model$seed-$ms
+  done
+
+  mkdir -p models/gc$seed-$ds-$ms
+  echo 'gc\n4 1\nmodels/g'$seed'-'$ms'\nmodels/c'$seed'-'$ms > models/gc$seed-$ds-$ms/config.txt
+  python main.py -p $cuda -r $seed train -mc models/gc$seed-$ds-$ms/config.txt\
+   -d data/ms-celebs/data2/train$seed.csv -ds $ds -ms $ms\
+   -s train_specs.txt models/gc$seed-$ds-$ms
+done
+
+# ds=500
+# for ms in 50 150 250 350 450; do
+#   mkdir -p models/gc$seed-$ds-$ms
+#   echo 'gc\n4 1\nmodels/g'$seed'-'$ms'\nmodels/c'$seed'-'$ms > models/gc$seed-$ds-$ms/config.txt
+#   python main.py -p $cuda -r $seed train -mc models/gc$seed-$ds-$ms/config.txt\
+#    -d data/mnist/train.csv -ds $ds -ms $ms\
+#    -s train_specs.txt models/gc$seed-$ds-$ms
+# done
+
+# ds=1000
+# for ms in 100 200 300 400 500 600 700 800 900 1000; do
+#   mkdir -p models/gc$seed-$ds-$ms
+#   echo 'gc\n4 1\nmodels/g'$seed'-'$ms'\nmodels/c'$seed'-'$ms > models/gc$seed-$ds-$ms/config.txt
+#   python main.py -p $cuda -r $seed train -mc models/gc$seed-$ds-$ms/config.txt\
+#    -d data/mnist/train.csv -ds $ds -ms $ms\
+#    -s train_specs.txt models/gc$seed-$ds-$ms
+# done
