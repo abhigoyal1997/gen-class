dfbea1bed709e8f7845237fdded0c45e9631e73c
Abhinav Goyal <agabohar@gmail.com>
2019-05-01 00:16:34 +0530

    scripts to run experiments

--- test_i.sh
+++ test_i.sh
@@ -1,9 +1,10 @@
 cuda=$1
 
+ds=468
 for seed in 0 1 2 3 4; do
-  for ms in 50 100 150 200 250 300 350 400 450 500; do
+  for ms in 47 94 140 187 234 281 328 374 421 468; do
     python main.py -p $cuda -r $seed test\
-     -d data/mnist/test.csv\
-     models/gc$seed-500-$ms -i
+     -d data/ms-celebs/test$seed.csv\
+     models/gc$seed-$ds-$ms -i
   done
 done
--- train_all.sh
+++ train_all.sh
@@ -1,18 +1,18 @@
 cuda=$1
 seed=$2
 
-ds=925
-for ms in 278 370 463 555 648 740 833 925; do
+ds=468
+for ms in 47 94 140 187 234 281; do
   for model in g c; do
     python main.py -p $cuda -r $seed train -mc $model'.config'\
-    -d data/ms-celebs/data2/train$seed.csv -ds $ms\
+    -d data/ms-celebs/train$seed.csv -ds $ms\
     -s train_specs.txt models/$model$seed-$ms
   done
 
   mkdir -p models/gc$seed-$ds-$ms
   echo 'gc\n4 1\nmodels/g'$seed'-'$ms'\nmodels/c'$seed'-'$ms > models/gc$seed-$ds-$ms/config.txt
   python main.py -p $cuda -r $seed train -mc models/gc$seed-$ds-$ms/config.txt\
-   -d data/ms-celebs/data2/train$seed.csv -ds $ds -ms $ms\
+   -d data/ms-celebs/train$seed.csv -ds $ds -ms $ms\
    -s train_specs.txt models/gc$seed-$ds-$ms
 done
 
